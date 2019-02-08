f245eab285e4d16dc18b6a5399ca5d5023da1998
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-02-08 11:50:49 +0100

    Create APIkeys_fetching.py

---
+++ APIkeys_fetching.py
@@ -0,0 +1,48 @@
+from Crypto.Cipher import AES  # pip install pycrypto
+import base64
+import getpass
+import json
+
+decrypt_dictionnary=''
+MDP=''
+
+def fetch_APIkeys(filename="APIkeys.json"):
+    global decrypt_dictionnary
+    with open(filename, 'r') as f:
+        for line in f:
+        line_file= line_file + line
+        return line_file
+
+
+
+
+
+
+def askPassword():
+    global MDP
+	MDP=getpass.getpass(prompt='Password:')
+
+def cypher_aes(secret_key, msg_text, encrypt=True):
+    # an AES key must be either 16, 24, or 32 bytes long
+    # in this case we make sure the key is 32 bytes long by adding padding and/or slicing if necessary
+    remainder = len(secret_key) % 16
+    modified_key = secret_key.ljust(len(secret_key) + (16 - remainder))[:32]
+    print(modified_key)
+
+    # input strings must be a multiple of 16 in length
+    # we achieve this by adding padding if necessary
+    remainder = len(msg_text) % 16
+    modified_text = msg_text.ljust(len(msg_text) + (16 - remainder))
+    print(modified_text)
+
+    cipher = AES.new(modified_key, AES.MODE_ECB)  # use of ECB mode in enterprise environments is very much frowned upon
+
+    if encrypt:
+        return base64.b64encode(cipher.encrypt(modified_text)).strip()
+
+
+
+def main():
+    askPassword()
+    decrypt_dictionnary=cypher_aes(MDP, fetch_APIkeys(filename="APIkeys.json"), encrypt=True)
+    APIkeys=json.dumps(decrypt_dictionnary)
