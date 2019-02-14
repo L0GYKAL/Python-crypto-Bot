7573df679053f280f610b91ae9789ecc5488ff5f
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-02-14 19:58:11 +0100

    Create CoreSoft.py

---
+++ CoreSoft.py
@@ -0,0 +1,16 @@
+import APIkeys_fetching.py
+import ccxt
+
+exchanges=[]
+def openExchange():
+  global exchanges
+  for exchange, data in decrypt_dictionnary.items():
+    exchanges.append(
+    exec(“exchanges.append(%s = %d.%e)” %(data[0], 'ccxt',data[0]))
+
+
+
+def main():
+  for exchange in exchanges:
+    exchange.fetchBalance
+    exchange.fetchMyTrades
