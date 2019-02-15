c796d6c2d6ba4ea046d7fe7143b84f5ee496d51d
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-02-15 19:18:22 +0100

    Created run() method
    self.run(Pass) give the aility to give the password to the class from another instance

--- APIkeys_fetching.py
+++ APIkeys_fetching.py
@@ -1,39 +1,49 @@
 from Crypto.Cipher import AES  # pip install pycrypto
-import base64 # pip install base64
-import getpass # pip install getpass
+import base64  # pip install base64
 import json
 import os
+
 
 class APIkeys:
 
     def __init__(self):
-        self.exchanges=[]
+        self.exchanges = []
         self.decrypt_dictionnary = ''
         self.Pass = ''
-        self.fileName='APIkeys.json'
+        self.fileName = 'APIkeys.json'
         self.fetch_APIkeys()
+
+        # def askPassword(self):
+        #self.Pass = getpass.getpass(prompt='Password:')
+
+    def run(self, Pass):
+        self.Pass = Pass
+        self.fetch_APIkeys()
+
     def fetch_APIkeys(self):
         if os.path.isfile(self.fileName):
             with open(self.fileName, 'r') as f:
-                line_file=''
+                line_file = ''
                 for line in f:
                     line_file = line_file + line
                 return line_file
         else:
             myfile = open(self.fileName, 'a')
             myfile.close()
-            return cypher_aes(self.Pass, '{}', encrypt=True)
+            return self.cypher_aes(self.Pass, '{}', encrypt=True)
+
     def write_APIkeys(self):
         with open(self.fileName, 'w') as f:
-            crypt_dictionnary = self.cypher_aes(self.Pass, self.decrypt_dictionnary, encrypt=True)
+            crypt_dictionnary = self.cypher_aes(
+                self.Pass, self.decrypt_dictionnary, encrypt=True)
             f.writelines(crypt_dictionnary)
-    def askPassword(self):
-        self.Pass = getpass.getpass(prompt='Password:')
+
     def cypher_aes(secret_key, msg_text, encrypt=True):
         # an AES key must be either 16, 24, or 32 bytes long
         # in this case we make sure the key is 32 bytes long by adding padding and/or slicing if necessary
         remainder = len(secret_key) % 16
-        modified_key = secret_key.ljust(len(secret_key) + (16 - remainder))[:32]
+        modified_key = secret_key.ljust(
+            len(secret_key) + (16 - remainder))[:32]
 
         # input strings must be a multiple of 16 in length
         # we achieve this by adding padding if necessary
@@ -48,39 +58,39 @@ class APIkeys:
 
         return cipher.decrypt(base64.b64decode(modified_text)).strip()
 
-
     # MANAGE apikeys
     # les paramètres sont des strings
+
     def add_APIkeys(self, exchangeName, exchange, publicKey, secretKey):
         """Permet de vérifier si l'exchange existe déjà ou pas
         et ajouter l'exchange au dictionnaire puis l'écrire dans le dictionnaire crypté"""
         if self.exchangeExist(exchangeName, exchange, publicKey, secretKey):
             print('This exchange already exist. Try to edit it')
         else:
-            self.decrypt_dictionnary[exchangeName] = [exchange, publicKey, secretKey]
+            self.decrypt_dictionnary[exchangeName] = [
+                exchange, publicKey, secretKey]
             self.write_APIkeys()
 
     def edit_APIKeys(self, exchangeName, exchange, publicKey, secretKey):
         """Permet de vérifier si l'exchange existe déjà ou pas
         et éditer l'exchange sur le dictionnaire puis l'écrire dans le dictionnaire crypté"""
         if self.exchangeExist(exchangeName, exchange, publicKey, secretKey):
-            self.decrypt_dictionnary[exchangeName] = [exchange, publicKey, secretKey]
+            self.decrypt_dictionnary[exchangeName] = [
+                exchange, publicKey, secretKey]
             self.write_APIkeys()
         else:
             print("This exchange doesn't exist!")
 
-    def del_APIkeys(self,exchangeName):
+    def del_APIkeys(self, exchangeName):
         if self.exchangeExist(exchangeName):
             del self.decrypt_dictionnary[exchangeName]
         else:
             print('There is no exchange named: ' + exchangeName)
-        write_APIkeys(self.decrypt_dictionnary)
+        self.write_APIkeys()
 
     def decrypt_APIKeys(self):
-        self.askPassword()
         self.decrypt_dictionnary = json.dumps(self.cypher_aes(
             self.MDP, self.fetch_APIkeys(self.fileName), encrypt=False))
-
 
     def exchangeExist(self, exchangeName='', exchange='', publicKey='', secretKey=''):
         """regarde si l'exchange existe déjà, en regardant si les nom donné par l'utilisateur
@@ -89,6 +99,6 @@ class APIkeys:
             if ((exchangeNames == exchangeName) or (data[2] == secretKey and data[1] == publicKey and data[0] == exchange)):
                 return True
         return False
-    
-    def get(self):  #la methode get à été créée pour garder l'encapsulation dans la programmation orientée objet
+
+    def get(self):  # la methode get à été créée pour garder l'encapsulation dans la programmation orientée objet
         return self.decrypt_dictionnary
