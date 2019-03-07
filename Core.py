70f375a867d3459c4b325a0c9144d589a08ffc07
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-03-07 12:01:07 +0100

    Update Core.py

--- Core.py
+++ Core.py
@@ -13,11 +13,21 @@ class Exchanges:
       for exchange, data in self.dictionnary.items():
         exec(“self.exchangesList.append(%s = %d.%e(%f,%g))” %(data[0], 'ccxt', data[0], data[1], data[2]))
 
+def balances(Exchanges_balances: list) -> int: #changer le nom de la variable, liste de dictionnaires avec les soldes de chaque compte en différentes crypto
+    for balance in Exchanges_balances:
+        for crypto in balance:
+            TotalBTC=amount*priceInBTC
+            TotalUSD=amount*priceInUSD
+            TotalEUR=amount*priceInEUR
+    
+    
 
 
 def main():
-  knownExchenges=Exchanges()
-  openExchanges()
+  knownExchanges=Exchanges()
+  knownExchanges.openExchanges()
   for exchange in exchanges:
-    exchange.fetchBalance
-    exchange.fetchMyTrades
+    exchange.fetchBalance()
+    exchange.fetchMyTrades()
+   
+
