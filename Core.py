fae970084be6924860cc1179710d88f921fea150
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-03-07 18:10:24 +0100

    Update Core.py

--- Core.py
+++ Core.py
@@ -3,15 +3,18 @@ import ccxt
 
 class Exchanges:
     def __init__(self):
-        self.exchangesList=[]
+        self.exchangesList=generateExchanges()
         self.APIobject=APIKeys()
         #demande le mot de passe
         self.APIobject.run('Mot de Passe')
         self.dictionnary=self.APIobject.get()
+        
 
-    def openExchanges(self):
+    def generateExchanges(self):
+      exchangesList=[]
       for exchange, data in self.dictionnary.items():
-        exec(“self.exchangesList.append(%s = %d.%e(%f,%g))” %(data[0], 'ccxt', data[0], data[1], data[2]))
+        exec(“self.exchangesList.append(%s = %d.%e(%f,%g,{'enableRateLimit': True,}))” %(data[0], 'ccxt', data[0], data[1], data[2]))
+      return exchangesList
 
 def balances(Exchanges_balances: list) -> int: #changer le nom de la variable, liste de dictionnaires avec les soldes de chaque compte en différentes crypto
     for balance in Exchanges_balances:
