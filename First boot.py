91a6ca1ed6259373234d92f87330bb4c315089ae
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-03-08 11:15:40 +0100

    First iteration of first boot

--- First boot.py
+++ First boot.py
@@ -1,1 +1,12 @@
-
+main():
+  if Apikeys == json.dumps('{}'):
+  #c'est la première fois que le programme se lance
+  """Il faut que l'interface demande:
+        Un Username
+        Un MDP et une confirmation du MDP"""
+  while Pass1 != Pass2:
+    #l'interface dit 'passwords don't match'
+  global MDP
+  MDP=str(Pass1)
+  #Lancer la fonction tuto
+  Tuto()
