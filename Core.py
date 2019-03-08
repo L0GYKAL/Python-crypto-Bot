cbe859e61740a84123c114925f298583c39ad79e
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-03-08 21:08:19 +0100

    Update Core.py

--- Core.py
+++ Core.py
@@ -1,59 +1,48 @@
 import APIkeys_fetching.py
-import ccxt
 import pandas as pd
+import ccxt
 
-MDP=''
-exchangesList=''
-""" l'interface graphique demande un MDP (AskPass(FirstTime))
-    déchiffrement du dictionnaire
-    stockage des info dans une pandas datastructure
-    création des objets exchanges avec ccxt
-    liste des exchanges -> pandas datastructure (dernière colonne)
-
-    global MDP, exchangesList
-    """
 def boot():
-    #lancer l'interface graphique -> MDP
-    Api=APIKeys()
-    ApiDict = ApiDict.get()
-    exchangesList = pd.dataframe(ApiDict, index=index)
+    global MDP, exchangesDict, exchangesDf
+    #MDP  =  lancer l'interface graphique, si c'est la première fois qu'il lance le programme -> MDP 
+    if firstTime== True:
+        firstboot()
+    APIobject=APIKeys()
+    APIobject.run(MDP)
+    exchangesDict=APIobject.get()
+    exchangesDf = pandas.DataFrame.from_dict(exchangesDict , orient = 'index', columns = ['Type','PublicKey', 'PrivateKey'])
+    openExchanges()
+    
 
-
-
-
-
-
-
-class Exchanges:
-    def __init__(self):
-        self.exchangesList=generateExchanges()
-        self.APIobject=APIKeys()
-        #demande le mot de passe
-        self.APIobject.run('Mot de Passe')
-        self.dictionnary=self.APIobject.get()
-
-
-    def generateExchanges(self):
-      exchangesList=[]
-      for exchange, data in self.dictionnary.items():
-        exec(“self.exchangesList.append(%s = %d.%e(%f,%g,{'enableRateLimit': True,}))” %(data[0], 'ccxt', data[0], data[1], data[2]))
-      return exchangesList
-
-def balances(Exchanges_balances: list) -> int: #changer le nom de la variable, liste de dictionnaires avec les soldes de chaque compte en différentes crypto
-    for balance in Exchanges_balances:
-        for crypto in balance:
-            TotalBTC=amount*priceInBTC
-            TotalUSD=amount*priceInUSD
-            TotalEUR=amount*priceInEUR
-
-
-
-
-def main():
-  knownExchanges=Exchanges()
-  knownExchanges.openExchanges()
-  for exchange in exchanges:
-    exchange.fetchBalance()
-    exchange.fetchMyTrades()
-
-
+def first():
+    if Apikeys == json.dumps('{}'):
+        return True
+    else:
+        return False
+    
+def firstboot():
+    """Il faut que l'interface demande:
+        Un Username
+        Un MDP et une confirmation du MDP"""
+    while Pass1 != Pass2:
+      #l'interface dit 'passwords don't match'
+    global MDP
+    MDP=str(Pass1)
+    #Lancer la fonction tuto
+    Tuto()#affiche un tutoriel
+  
+    
+    
+def openExchanges():
+    global exchangesDf
+    exchangeSerie = pd.Series(dtype = ccxt)
+    for i in exchangesDf.index:
+        code =  'exchangeSerie.set_value(' +i + ',' + exchangesDf.index[i] + "ccxt." + exchangesDf.Type[i] + "({" + exchangesDf.PublicKey[i] + ',' + exchangesDf.PrivateKey[i] + '}))'
+        exec(code)
+    exchangesDf.append(exchangeSerie)
+    
+    
+    
+    
+if __name__ == '__main__':
+    boot()
