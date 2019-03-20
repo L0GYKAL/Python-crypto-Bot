b7af2d4ac445d12ca07da95768c6c9b4f807b3cb
Godlike74 <32228897+Godlike74@users.noreply.github.com>
2019-03-20 20:17:47 +0100

    Update Balances.py

--- Balances.py
+++ Balances.py
@@ -1,43 +1,39 @@
-#THIS WORKS
+# THIS WORKS
 import ccxt
 import pandas as pd
-total = float()
-binance = ccxt.binance({
-   'id':
-   'Binance',
-   'apiKey':
-   '',
-   'secret':
-   ''
-})
-balances = binance.fetchBalance()
-# print(balances)
-balances = pd.DataFrame(data=balances['info']['balances'])
-# print(balances)
-for i in balances.index:
-   if balances.loc[i, 'asset'] == 'BTC':
-       balInBTC = float(balances.loc[i, 'free']) + float(
-           balances.loc[i, 'locked'])
-       print('You have ' + str(
-           float(balances.loc[i, 'free']) + float(balances.loc[i, 'locked'])
-       ) + ' ' + balances.loc[i, 'asset'] + ' It represents ' + str(balInBTC))
-       total += balInBTC
-   elif balances.loc[i, 'asset'] == 'VTHO':
-       pass
-   elif float(balances.loc[i, 'free']) + float(
-           balances.loc[i, 'locked']) != 0:
-try:
-       ticker = binance.fetchTicker(balances.loc[i, 'asset'] + '/BTC')
-except:
-     ticker['last']=0
-       balInBTC = ticker['last'] * (
-           float(balances.loc[i, 'free']) + float(balances.loc[i, 'locked']))
-       print('You have ' + str(
-           float(balances.loc[i, 'free']) + float(balances.loc[i, 'locked'])
-       ) + ' ' + balances.loc[i, 'asset'] + ' It represents ' + str(balInBTC))
-       total += balInBTC
-print('You have an aproximated amount of BTC: '+ total)
-#reste a vérifier si la paires existe dans le market  avec fetchMarkets()
-"""
-   
-"""
+
+
+def fetchBalance(exchange):
+    total = float()
+# exchange[0]=ccxtObject and exchange[1]=name
+    """binance = ccxt.binance({
+        'id':
+        'Binance',
+        'apiKey':
+        'QDvA3MvcZNgy0kFmsSFlBwX3VirMcV5VnhjOctTRqohH46Ces8glcRys48H8ddwX',
+        'secret':
+        'yISA8ODctEZY4ncjpHIxAKar638Xe9hvjmldi7TRKxQ9L1Zcb3MvSuJMDpeIG8rs'
+    })"""
+    balances = exchange[0].fetchBalance()
+    # print(balances)
+    balances = pd.DataFrame(data=balances['info']['balances'])
+    exchange[0].loadMarkets()
+    for i in balances.index:
+        ticker = balances.loc[i, 'asset'] + '/BTC'
+        if exchange[0].markets[ticker]:
+            if float(balances.loc[i, 'free']) + float(
+                    balances.loc[i, 'locked']) != 0:
+                if balances.loc[i, 'asset'] == 'BTC':
+                    balInBTC = float(balances.loc[i, 'free']) + float(
+                        balances.loc[i, 'locked'])
+                else:
+                    balInBTC = ticker['last'] * (
+                        float(balances.loc[i, 'free']) + float(balances.loc[i, 'locked']))
+        print('You have ' + str(
+            float(balances.loc[i, 'free']) +
+            float(balances.loc[i, 'locked'])
+        ) + ' ' + balances.loc[i, 'asset'] + ' It represents ' + str(balInBTC))
+        total += balInBTC
+    print('You have an approximate amount of' +
+          total + ' BTC in ' + exchange[1])
+    # reste a vérifier si la paires existe dans le market  avec loadMarkets()
