9f3656fc211e26bcbcb9b783a650ae93009af8e5
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-03-28 18:54:41 +0100

    Verifying the pair

--- Balances.py
+++ Balances.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 18:47:56 2019
+
+@author: LOGYKAL
+"""
+
 # THIS WORKS
 import ccxt
 import pandas as pd
 import requests
 
 
-def fetchAddress(symbol, address):     #fetchAddress('doge', 'DSFi6NPHgt3R8Jr2HJyrSq35QtuRsUEGxm')
+def fetchAddress(symbol, address):  # fetchAddress('doge', 'DSFi6NPHgt3R8Jr2HJyrSq35QtuRsUEGxm')
     """ Il faut faire une liste défilante avec les asset qui sont retournés par la request ci-dessus
     symbolList = requests.get('https://api.hybrix.io/asset/').json()"""
     request = requests.get(
@@ -17,8 +24,17 @@ def fetchAddress(symbol, address):     #fetchAddress('doge', 'DSFi6NPHgt3R8Jr2HJ
         print('There is a probleme')
 
 
-def fetchBalance(exchange):
-    total = float()
+def convertAdress(symbol: str, amount: float, conversion: str) -> float:
+    """symbol: BTC
+    amount:0.25968
+    conversion: EUR"""
+    request = requests.get('https://api.cryptonator.com/api/full/' + symbol + '-' + conversion)
+    weightedPrice= request['ticker']['price']
+    return weightedPrice
+
+
+def fetchExchangeBalance(exchange):
+    total=float()
 # exchange[0]=ccxtObject and exchange[1]=name
     """binance = ccxt.binance({
         'id':
@@ -28,28 +44,26 @@ def fetchBalance(exchange):
         'secret':
         'yISA8ODctEZY4ncjpHIxAKar638Xe9hvjmldi7TRKxQ9L1Zcb3MvSuJMDpeIG8rs'
     })"""
-    balances = exchange[0].fetchBalance()
+    balances=exchange[0].fetchBalance()
     # print(balances)
-    balances = pd.DataFrame(data=balances['info']['balances'])
-    exchange[0].loadMarkets()
+    balances=pd.DataFrame(data=balances['info']['balances'])
+    markets = exchange[0].loadMarkets()
     for i in balances.index:
-        ticker = balances.loc[i, 'asset'] + '/BTC'
-        if exchange[0].markets[ticker]:
-            if float(balances.loc[i, 'free']) + float(
-                    balances.loc[i, 'locked']) != 0:
-                if balances.loc[i, 'asset'] == 'BTC':
-                    balInBTC = float(balances.loc[i, 'free']) + float(
-                        balances.loc[i, 'locked'])
-                else:
-                    balInBTC = ticker['last'] * (
-                        float(balances.loc[i, 'free']) + float(balances.loc[i, 'locked']))
-        print('You have ' + str(
-            float(balances.loc[i, 'free'])
-            + float(balances.loc[i, 'locked'])
-        ) + ' ' + balances.loc[i, 'asset'] + ' It represents ' + str(balInBTC))
-        total += balInBTC
+        ticker=balances.loc[i, 'asset'] + '/BTC'
+            if exchange[0].markets[ticker]:
+                if float(balances.loc[i, 'free']) + float(
+                        balances.loc[i, 'locked']) != 0:
+                    if balances.loc[i, 'asset'] == 'BTC':
+                        balInBTC=float(balances.loc[i, 'free']) + float(
+                            balances.loc[i, 'locked'])
+                    else:
+                        if markets[ticker] != 0:
+                            balInBTC=ticker['last'] * (
+                                float(balances.loc[i, 'free']) + float(balances.loc[i, 'locked']))
+            print('You have ' + str(
+                float(balances.loc[i, 'free'])
+                + float(balances.loc[i, 'locked'])
+            ) + ' ' + balances.loc[i, 'asset'] + ' It represents ' + str(balInBTC))
+            total += balInBTC
     print('You have an approximate amount of'
           + total + ' BTC in ' + exchange[1])
-    # reste a vérifier si la paires existe dans le market  avec loadMarkets()
-
-
