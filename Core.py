3d6a5e74c479810cd3b13a4f6a188f8dd2d315cd
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-04-12 10:38:17 +0200

    Update Core.py

--- Core.py
+++ Core.py
@@ -3,43 +3,7 @@ import tutorial.py
 import FirstBoot.py
 import pandas as pd
 import ccxt
-
-
-def boot():
-    global MDP, exchangesDict, exchangesDf
-    # MDP  =  lancer l'interface graphique, si c'est la première fois qu'il lance le programme -> MDP
-    if firstTime == True:
-        firstboot()
-    APIobject = APIKeys()
-    APIobject.run(MDP)
-    exchangesDict = APIobject.get()
-    exchangesDf = pandas.DataFrame.from_dict(exchangesDict, orient='index', columns=[
-                                             'Type', 'PublicKey', 'PrivateKey'])
-    openExchanges()
-
-
-def openExchanges():
-    global exchangesDf
-    exchangeSerie = pd.Series(dtype=ccxt)
-    for i in exchangesDf.index:
-        code = 'exchangeSerie.set_value(' + i + ',' + exchangesDf.index[i] + "ccxt." + exchangesDf.Type[i] + \
-            "({" + exchangesDf.PublicKey[i] + \
-            ',' + exchangesDf.PrivateKey[i] + '}))'
-        exec(code)
-    exchangesDf.append(exchangeSerie)
-
-
-if __name__ == '__main__':
-    boot()
-    
-    
-    
-#deuxième version
-import APIkeys_fetching.py
-import tutorial.py
-import FirstBoot.py
-import pandas as pd
-import ccxt
+import sys
 
 
 def boot():
@@ -67,4 +31,8 @@ def openExchanges():
 
 
 if __name__ == '__main__':
+    if not sys.version >= '3.6':
+        print('This script requires Python 3.6+')
+        sys.exit()
+    else:
     boot()
