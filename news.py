fada16a8f0edab90678ab199f7bf57bbc0e0a3db
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2019-05-05 19:06:24 +0200

    Update news.py

--- news.py
+++ news.py
@@ -1,13 +1,16 @@
-import requests
+import requests #api
+from bs4 import BeautifulSoup #parser html
 
 
-def news():
+
+
+def news():#recupère les news par API et créer une liste de liste avec les informations comme le titre, le corps, la source et la date
     response = requests.get('https://data.messari.io/api/v1/news').json()
     articles = list()
-    for news in response:
-        title = response[news]['data']['title']
-        content = response[news]['data']['content']
-        source = response[news]['data']['references']['url']
-        date = response[news]['data']['published_at']
-        articles.append(title, content, source, date)
-    return articles
+    for news in response['data']:
+        title = BeautifulSoup(news['title'], "html.parser").get_text()
+        content = BeautifulSoup(news['content'], "html.parser").get_text()
+        source = news['references'][0]['url']
+        date = BeautifulSoup(news['published_at'], "html.parser").get_text()
+        articles.append([title, content, source, date])
+    return (articles)
