acd422a465bef7bb4aa9dee1f6a85b54db0b11ad
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2019-05-05 19:17:00 +0200

    Update Core.py

--- Core.py
+++ Core.py
@@ -1,6 +1,6 @@
 import APIkeys_fetching.py
 import tutorial.py
-import GUI.LoginRegistration.py
+import GUI.mainscreen3final.py
 import pandas as pd
 import ccxt
 import sys
@@ -10,14 +10,15 @@ def boot():
     global MDP, exchangesDf
 
     if is_connected():
-      APIobject = APIKeys()
-       if APIobject.firstTime() == True:  # c'est la première fois que ça a été lancé
+        APIobject = APIKeys()
+        if APIobject.firstTime() == True:  # c'est la première fois que ça a été lancé
             register_user()  # MDP  =  lancer l'interface graphique, si c'est la première fois qu'il lance le programme -> MDP
             APIobject.run(MDP)
         else:
             login()
-            while not pass(MDP):
+            while (APIobject.password(MDP)) != True:
                 login()
+                APIobject.password(MDP)
             APIobject.run(MDP)
         # openExchanges()
     else:
@@ -40,4 +41,4 @@ if __name__ == '__main__':
         print('This script requires Python 3.6+')
         sys.exit()
     else:
-    boot()
+        boot()
