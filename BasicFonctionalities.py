430ee51afee8d3e68fe65a931bc5bd862adffb2a
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-06 18:50:22 +0200

    Update BasicFonctionalities.py

--- BasicFonctionalities.py
+++ BasicFonctionalities.py
@@ -9,3 +9,25 @@ def is_connected():  # retrun True if connected, False else
     except OSError:
         pass
     return False
+
+def tickerFinder(symbol: str):
+    markets = dict()
+    certified = ['upbit', 'kucoin', 'kraken', 'coss', 'bittrex', 'bitfinex', 'binance']
+    ccxtList = []
+    for exchange in certified:
+        exec("ccxtList.append(ccxt." + str(exchange) + "())")
+    for exchange in certified: #ticker in certified[exchange]...
+        i=0
+        markets[exchange] = ccxtList[i].loadMarkets()#trouver tous les tickers
+        i+=1
+    for exchange in certified:
+        tickers = []
+        for ticker in markets[exchange].keys():
+            if str(ticker).find(symbol) != -1:
+                tickers.append(str(ticker))
+        markets[exchange] = tickers
+    return markets
+
+if __name__ == '__main__':
+    if is_connected():
+        print(tickerFinder('OMG'))
