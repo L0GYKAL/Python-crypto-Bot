51dc3a49777f4f78a7fae31d84d61d32eb4d376f
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-06 21:42:47 +0200

    Handling errors

--- news.py
+++ news.py
@@ -8,9 +8,22 @@ def news():#recupère les news par API et créer une liste de liste avec les inf
     response = requests.get('https://data.messari.io/api/v1/news').json()
     articles = list()
     for news in response['data']:
-        title = BeautifulSoup(news['title'], "html.parser").get_text()
+        try:
+            title = BeautifulSoup(news['title'], "html.parser").get_text()
+        except:
+            title = "None"
+        try:
         content = BeautifulSoup(news['content'], "html.parser").get_text()
+        except:
+            content = "None"
+        try:
         source = news['references'][0]['url']
+        except:
+            news = "None"
+        try:
         date = BeautifulSoup(news['published_at'], "html.parser").get_text()
+        except:
+            date ="None"
         articles.append([title, content, source, date])
+
     return (articles)
