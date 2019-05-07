14edc966b7bf6480d99118bcff88baf0a7ee453e
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-07 16:30:34 +0200

    Update news.py

--- news.py
+++ news.py
@@ -1,10 +1,8 @@
-import requests #api
-from bs4 import BeautifulSoup #parser html
+import requests  # api
+from bs4 import BeautifulSoup  # parser html
 
 
-
-
-def news():#recupère les news par API et créer une liste de liste avec les informations comme le titre, le corps, la source et la date
+def news():  # recupère les news par API et créer une liste de liste avec les informations comme le titre, le corps, la source et la date
     response = requests.get('https://data.messari.io/api/v1/news').json()
     articles = list()
     for news in response['data']:
@@ -13,17 +11,18 @@ def news():#recupère les news par API et créer une liste de liste avec les inf
         except:
             title = "None"
         try:
-        content = BeautifulSoup(news['content'], "html.parser").get_text()
+            content = BeautifulSoup(news['content'], "html.parser").get_text()
         except:
             content = "None"
         try:
-        source = news['references'][0]['url']
+            source = news['references'][0]['url']
         except:
             news = "None"
         try:
-        date = BeautifulSoup(news['published_at'], "html.parser").get_text()
+            date = BeautifulSoup(news['published_at'],
+                                 "html.parser").get_text()
         except:
-            date ="None"
+            date = "None"
         articles.append([title, content, source, date])
 
     return (articles)
