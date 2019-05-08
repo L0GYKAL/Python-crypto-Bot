3d238e48339942bc6573a8b9022716a83c442701
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-08 11:38:24 +0200

    Update BasicFonctionalities.py

--- BasicFonctionalities.py
+++ BasicFonctionalities.py
@@ -1,26 +1,31 @@
-import socket
 import requests
+import ccxt
 
 
 def is_connected():  # retrun True if connected, False else
+    url = 'http://www.google.com/'
+    timeout = 5
     try:
         # connect and tell us if the host is reachable
-        socket.create_connection(‘www.google.com’, 80)
+        _ = requests.get(url, timeout=timeout)
         return True
-    except OSError:
-        pass
+    except requests.ConnectionError:
+        print("İnternet bağlantısı yok.")
     return False
+
 
 def tickerFinder(symbol: str):
     markets = dict()
-    certified = ['upbit', 'kucoin', 'kraken', 'coss', 'bittrex', 'bitfinex', 'binance']
+    certified = ['upbit', 'kucoin', 'kraken',
+                 'coss', 'bittrex', 'bitfinex', 'binance']
     ccxtList = []
     for exchange in certified:
         exec("ccxtList.append(ccxt." + str(exchange) + "())")
-    for exchange in certified: #ticker in certified[exchange]...
-        i=0
-        markets[exchange] = ccxtList[i].loadMarkets()#trouver tous les tickers
-        i+=1
+    for exchange in certified:  # ticker in certified[exchange]...
+        i = 0
+        # trouver tous les tickers
+        markets[exchange] = ccxtList[i].loadMarkets()
+        i += 1
     for exchange in certified:
         tickers = []
         for ticker in markets[exchange].keys():
@@ -28,6 +33,39 @@ def tickerFinder(symbol: str):
                 tickers.append(str(ticker))
         markets[exchange] = tickers
     return markets
+
+
+def marketPercent():
+    # récupération des informations ar API
+    info = requests.get('https://api.coinmarketcap.com/v1/ticker/').json()
+    percentages = []
+    # 7days
+    percentage7d = float()
+    totalpercent = float()
+    for i in info:
+        totalpercent += float(i['percent_change_7d'])
+    # division de la somme des pourcentages par toute les crypto
+    percentage7d = totalpercent / len(info)
+    # ajout à la liste du pourcentage arrondie
+    percentages.append(round(percentage7d, 4))
+    # 1day
+    percentage1d = float()
+    totalpercent = float()
+    for i in info:
+        totalpercent += float(i['percent_change_24h'])
+    percentage1d = totalpercent / len(info)
+    # ajout à la liste du pourcentage arrondie
+    percentages.append(round(percentage1d, 4))
+    # 1hour
+    percentage1h = float()
+    totalpercent = float()
+    for i in info:
+        totalpercent += float(i['percent_change_1h'])
+    percentage1h = totalpercent / len(info)
+    # ajout à la liste du pourcentage arrondie
+    percentages.append(round(percentage1h, 4))
+    # liste avec la moyenne des porncentages sur 7 jours, sur 24 heures et sur une heure
+    return percentages
 
 
 if __name__ == '__main__':
