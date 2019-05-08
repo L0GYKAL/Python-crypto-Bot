d20978c1448f9d494b333b291d4645de38dafd70
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-08 20:22:11 +0200

    Update orders.py

--- orders.py
+++ orders.py
@@ -19,35 +19,8 @@ def cancelOrder(exchange: ccxt, orderId, symbol: str): #ex: cancelOrder(binance,
     print('error')
   #(ex: cancelOrder(binance, 11480381, 'MFT/BTC'))
   
-
-#Trades terminés
-"""
-if exchange.has['fetchOrders']:
-    since = exchange.milliseconds () - 86400000  # -1 day from now
-    # alternatively, fetch from a certain starting datetime
-    # since = exchange.parse8601('2018-01-01T00:00:00Z')
-    all_orders = []
-    while since < exchange.milliseconds ():
-        symbol = None  # change for your symbol
-        limit = 20  # change for your limit
-        orders = await exchange.fetch_orders(symbol, since, limit)
-        if len(orders):
-            since = orders[len(orders) - 1]['timestamp']
-            all_orders += orders
-        else:
-            break"""
-
 #créer un ordre
-"""
-symbol = 'ETH/BTC'
-type = 'limit'  # or 'market', other types aren't unified yet
-side = 'sell'
-amount = 123.45  # your amount
-price = 54.321  # your price
-# overrides
-params = {
-    'stopPrice': 123.45,  # your stop price
-    'type': 'stopLimit',
-}
-order = exchange.create_order(symbol, type, side, amount, price, params)
-"""
+def createOrder(exchange: ccxt, symbol: str, amount: float, price: float, side: str, type: str):# (side= 'buy' or 'sell'), type = 'limit' or 'market'
+    #binance.create_order('RVN/BTC', 'limit', 'buy', amount = 1.0, price = 0.060154)
+    order = exchange.create_order(symbol, type, side, amount, price)
+    return order
