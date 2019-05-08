56704886f4afc0d7ce7b8f7ef09b16ccdf45f48e
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-08 18:43:58 +0200

    Update OHLV.py

--- OHLV.py
+++ OHLV.py
@@ -1,3 +1,6 @@
+from dateutil.parser import parse
+import time
+import pandas as pd
 import ccxt
 import plotly.graph_objs as go
 import plotly.offline as py
@@ -21,6 +24,33 @@ def chart(exchange: ccxt, ticker: str, time):  # time: '1m','1d'
     return plot
 
 
+def BTC_liveGraph():
+    global plot_url  # pour afficher le graph, il faut utiliser cette URL
+    # déclaration des constantes
+    bitmex = ccxt.bitmex({'enableRateLimit': True})
+    layout = {'title': 'BTC live price',
+              'yaxis': {'title': 'Price in USD'}}
+    filename = 'BTC_liveGraph.html'
+    # récupération des premières info
+    ticker = bitmex.fetchTicker('BTC/USD')
+    datetime = parse(ticker['datetime']).time()
+    price = ticker['last']
+    # déclaration de la dataframe pandas
+    df = pd.DataFrame(columns=['time', 'price'], data=[[datetime, price]])
+    # boucle d'update
+    while True:
+        time.sleep(bitmex.rateLimit * 2 / 1000)  # time.sleep wants seconds
+        ticker = bitmex.fetchTicker('BTC/USD')
+        datetime = parse(ticker['datetime']).time()
+        price = ticker['last']
+        df.loc[len(df)] = [datetime] + [price]
+        trace = go.Scatter(x=df['time'], y=df['price'])
+        data = [trace]
+        fig = dict(data=data, layout=layout)
+        plot_url = py.plot(fig, filename=filename, auto_open=False)
+
+
 if __name__ == '__main__':
-    #binance = ccxt.binance()
-    chart(binance, 'RVN/BTC', '1d')
+    """binance = ccxt.binance()
+    chart(binance, 'RVN/BTC', '1d')"""
+    BTC_liveGraph()
