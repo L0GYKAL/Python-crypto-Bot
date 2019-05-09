fcffdb5e5e367de7052a027eaee13ca1e9e41bd7
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-09 19:35:03 +0200

    Update BasicFonctionalities.py

--- BasicFonctionalities.py
+++ BasicFonctionalities.py
@@ -1,12 +1,19 @@
 import requests
 import ccxt
+import pandas as pd
+import tweepy  # wrapper api de tweeter # https://developer.twitter.com/en/docs.html
+# module d'analyse sentimental
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-def getAllCurrencies():#return all symbols of the most known cryptocurrencies
-    response = requests.get(url = 'https://api.coinmarketcap.com/v1/ticker/').json()
+
+def getAllCurrencies():  # return all symbols of the most known cryptocurrencies
+    response = requests.get(
+        url='https://api.coinmarketcap.com/v1/ticker/').json()
     currencies = []
     for currency in response:
         currencies.append(currency['symbol'])
     return currencies
+
 
 def is_connected():  # retrun True if connected, False else
     url = 'http://www.google.com/'
@@ -72,6 +79,37 @@ def marketPercent():
     percentages.append(round(percentage1h, 4))
     # liste avec la moyenne des porncentages sur 7 jours, sur 24 heures et sur une heure
     return percentages
+
+
+def sentimentAnalysis(symbol: str)-> list:  # ex: symbol = BTC
+    # My Twitter API Authentication Variables
+    consumer_key = 'KWp3yqhwWdp0Fn9AcHx58878E'
+    consumer_secret = 'ABjkYLIDcc94MMQqQaCZ7eyrFkkoHiFpijRCjN7T3DfOuFb0JG'
+    access_token = '2210545440-qoCIdDpU5k7HSNzC3Kz2LDMmOgGMWrotTwkq4qy'
+    access_token_secret = 'P8J4IRpOPRXgskDTRCNGw94A6OJ0iA2ORBAJJOvFJ8LLG'
+
+    # authentification
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    # récupération des tweets
+    tweets = api.search(str('$' + symbol), count=200)
+
+    # création d'un dataframe avec les tweets
+    data = pd.DataFrame(
+        data=[tweet.text for tweet in tweets], columns=['Tweets'])
+    sid = SentimentIntensityAnalyzer()
+    sentiments = []
+
+    for index, row in data.iterrows():
+        ss = sid.polarity_scores(row["Tweets"])
+        sentiments.append(ss)
+
+    se = pd.Series(sentiments)
+    data['polarity'] = se.values
+    return data['polarity']
 
 
 if __name__ == '__main__':
