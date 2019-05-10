3a93856adb2cc6a7d8a4e52fac3e9534e4f77637
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-10 11:23:26 +0200

    Add files via upload

--- GUI/BTC_LiveGraph.py
+++ GUI/BTC_LiveGraph.py
@@ -6,8 +6,11 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
 import os
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+import graphics 
 
 class Ui_BTC_LiveGraph(object):
     def setupUi(self, BTC_LiveGraph):
@@ -17,6 +20,7 @@ class Ui_BTC_LiveGraph(object):
         self.htmlreader_BTCLiveGraph = QtWebEngineWidgets.QWebEngineView(BTC_LiveGraph)
         self.htmlreader_BTCLiveGraph.setGeometry(QtCore.QRect(10, 10, 1771, 981))
         self.htmlreader_BTCLiveGraph.setAccessibleDescription("")
+        #récupération du chemin absolu du fichier
         mypath = os.path.dirname(__file__)
         self.htmlreader_BTCLiveGraph.setUrl(QtCore.QUrl(str("file:///"+ mypath + "/BTC_liveGraph.html")))
         self.htmlreader_BTCLiveGraph.setObjectName("htmlreader_BTCLiveGraph")
@@ -27,8 +31,9 @@ class Ui_BTC_LiveGraph(object):
     def retranslateUi(self, BTC_LiveGraph):
         _translate = QtCore.QCoreApplication.translate
         BTC_LiveGraph.setWindowTitle(_translate("BTC_LiveGraph", "BTC Live Graph"))
+        graphics.BTC_liveGraph(self.htmlreader_BTCLiveGraph)
 
-from PyQt5 import QtWebEngineWidgets
+
 
 if __name__ == "__main__":
     import sys
