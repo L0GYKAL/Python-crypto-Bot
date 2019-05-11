ea392f554ad22adbc5492cc5ca08028d589cc9cc
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2019-05-11 19:12:03 +0200

    Update interfacefinalGUI.py

--- GUI/interfacefinalGUI.py
+++ GUI/interfacefinalGUI.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from TutorialGUI import Ui_Dialogue
-from newsGUI import Ui_News
+from news import Ui_News
 from watchlistGUI import Ui_Watchlist
 from ordersGUI import Ui_Orders
 from balanceaccount import Ui_balanceaccount
