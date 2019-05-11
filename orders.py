4346bd25877720eaaa39e3a068da7e827434beca
Mouran Antoine <32228897+Seigneur774@users.noreply.github.com>
2019-05-11 09:51:28 +0200

    Update orders.py

--- orders.py
+++ orders.py
@@ -1,7 +1,8 @@
 import ccxt
 
 #Ordres ouverts
-def openOrders(exchange: ccxt):
+def openOrders(exchange: str, apikey: str, secret: str):
+    exec('exchange = ccxt.' + exchange + "({'apikey':" + apikey + "'secret':" + secret + "})" )
     if exchange.has['fetchOpenOrders']:
         exchange.options["warnOnFetchOpenOrdersWithoutSymbol"] = False
         openOders = []
@@ -11,16 +12,18 @@ def openOrders(exchange: ccxt):
         return openOders#liste de liste [id, symbol, SELL/BUY, price, amount]
 
 #Cancel order:
-def cancelOrder(exchange: ccxt, orderId, symbol: str): #ex: cancelOrder(binance, 11480381, 'MFT/BTC')
-  try:
-    exchange.cancelOrder(orderId, symbol)
-    return str('Order canceled')
-  except: #if there is an error
-    print('error')
-  #(ex: cancelOrder(binance, 11480381, 'MFT/BTC'))
+def cancelOrder(exchange: str, orderId, symbol: str, apikey, secret): #ex: cancelOrder(binance, 11480381, 'MFT/BTC')
+    exec('exchange = ccxt.' + exchange + "({'apikey':" + apikey + "'secret':" + secret + "})" )
+    try:
+        exchange.cancelOrder(orderId, symbol)
+        return str('Order canceled')
+      except: #if there is an error
+        print('error')
+      #(ex: cancelOrder(binance, 11480381, 'MFT/BTC'))
   
 #créer un ordre
-def createOrder(exchange: ccxt, symbol: str, amount: float, price: float, side: str, type: str):# (side= 'buy' or 'sell'), type = 'limit' or 'market'
+def createOrder(exchange: str, symbol: str, amount: float, price: float, side: str, type: str):# (side= 'buy' or 'sell'), type = 'limit' or 'market'
     #binance.create_order('RVN/BTC', 'limit', 'buy', amount = 1.0, price = 0.060154)
+    exec('exchange = ccxt.' + exchange + '()')
     order = exchange.create_order(symbol, type, side, amount, price)
     return order
